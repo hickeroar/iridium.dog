6adeaa6860e5faa4a8f06818773c7a3f71b1096f
Ryan Vennell <ryan.vennell@gmail.com>
2017-10-17 21:20:02 -0500

    rearranging some files

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Main from "./Main";
 
-import logo from '../logo.svg';
-import '../App.css';
+import logo from '../assets/logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
