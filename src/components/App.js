0ff4bade76240a289a696e30c4bdf243947f382c
Ryan Vennell <ryan.vennell@gmail.com>
2017-10-20 17:33:38 -0500

    Site name tweak to trigger a build.

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to The Iridium Dog</h1>
+          <h1 className="App-title">The Iridium Dog</h1>
           <h4>
             <Link to="/">Home</Link>
             &nbsp;
